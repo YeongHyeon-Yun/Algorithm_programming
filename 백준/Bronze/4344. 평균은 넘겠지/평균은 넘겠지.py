2846bf6e74dd76dc59daaf9b70ba89810a49747a
YeongHyeon-Yun <90812508+YeongHyeon-Yun@users.noreply.github.com>
2022-09-29 12:42:51 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+for _ in range(num):
+
+    nums = list(map(int, input().split()))
+    avg = sum(nums[1:])/nums[0]
+    count = 0
+
+    for score in nums[1:]:
+        if score > avg:
+            count += 1
+    rate = count/nums[0] *100
+
+    print(f'{rate:.3f}%')
