d9800052551bdcf598bad65413ebac5bbc6cfe7b
YeongHyeon-Yun <90812508+YeongHyeon-Yun@users.noreply.github.com>
2023-01-10 09:26:56 +0900

    [unrated] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
+++ 프로그래머스/unrated/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -1,4 +1,4 @@
-select book.book_id, author.author_name, date_format(book.published_date, '%Y-%m-%d') from book book join author author
+select book.book_id, author.author_name, date_format(book.published_date, '%Y-%m-%d') from book join author
 on book.author_id = author.author_id
 where book.category = '경제'
 order by published_date;
