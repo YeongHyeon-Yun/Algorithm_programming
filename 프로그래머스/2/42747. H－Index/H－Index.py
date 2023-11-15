6f9bd7bcd8bb09ba2f859e2b5b45a39761b73344
YeongHyeon-Yun <90812508+YeongHyeon-Yun@users.noreply.github.com>
2023-11-15 13:46:50 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,12 +1,20 @@
-def solution(citations):
-    # print(citations)
-    citations.sort(reverse=True)
-    # print(citations)
-    answer = 0
+# def solution(citations):
+#     # print(citations)
+#     citations.sort(reverse=True)
+#     # print(citations)
+#     answer = 0
     
-    for i in range(len(citations)):
-        if(citations[i] < i+1):
-            answer = i
-            return answer
+#     for i in range(len(citations)):
+#         if(citations[i] < i+1):
+#             answer = i
+#             return answer
         
-    return len(citations)
+#     return len(citations)
+
+def solution(citations):
+    citations.sort(reverse=True)
+    # # a = map(min, enumerate(citations, start=1))
+    # a = enumerate(citations, start=1)
+    # print(a)
+    answer = max(map(min, enumerate(citations, start=1)))
+    return answer
